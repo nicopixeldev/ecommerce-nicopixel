ae51334ece46dab9d7eb12d36cb359d50b33913a
nicopixel <nicopixelsoftware@gmail.com>
2025-02-27 17:20:23 +0100

    redux + reselect + redux-persist + redux-thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,31 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
-import { createAction } from '../../utils/reducer/reducer.utils';
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailure = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+// this is a thunk, it is a function that returns a function
+// it is used to dispatch actions asynchronously
+// it should end with "Async" by convention
+export const fetchCategoriesStartAsync = () => {
+  return async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+      dispatch(fetchCategoriesFailure(error));
+    }
+  };
+};
