94f910a5b7901034aee3f4b9ddb686a28d5a198d
nicopixel <nicopixelsoftware@gmail.com>
2025-02-28 13:22:14 +0100

    chore: add some comments

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,36 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
-import { rootReducer } from './root-reducer';
+// here we define the root reducer with all the reducers
+import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
+// configuration for the Redux DevTools
+// Chrome extension: https://chromewebstore.google.com/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+  (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  blacklist: ['user'],
+  // we don't want to persist the user reducer into the local storage
+  blacklist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
+// create the store with the persisted reducer and the compose enhancer
+// preloadedState is not used in this case - undefined
 export const store = createStore(
   persistedReducer,
   undefined,
